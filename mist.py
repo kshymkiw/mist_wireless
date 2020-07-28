7adf9f285fdbf161831f286a20b305280c977650
Kevin Shymkiw <kshymkiw@gmail.com>
2020-07-28 12:55:55 -0600

    Add files via upload
    Added support for schedule, so the script runs every 30 seconds

--- mist.py
+++ mist.py
@@ -3,17 +3,43 @@ import json
 import time
 import os
 import string
-# Set Parameters you need to gather data from the Mist REST API
-OrgID = '<Your ORG ID>'
-AuthToken = '<Your Auth Token>'
-URL = "https://api.mist.com/api/v1/orgs/%s/stats" % (OrgID)
-# Define the Headers for Token Auth
+import schedule
+
+### Set Variables
+OrgID = '<Your OrgID from Mist Dashboard>'
+AuthToken = '<Your AuthToken from Mist API>'
+url = "https://api.mist.com/api/v1/orgs/%s/stats" % (OrgID)
+
+payload = {}
 headers = {
-	'Content-Type': 'application/json',
-	'Authorization': 'Token {}'.format(AuthToken)
+  'Authorization': 'Token {}'.format(AuthToken),
+  'Content-Type': 'application/json'
 }
-# Make a GET Request and get HTTP Response Code
-response = requests.get(URL, headers=headers)
-data = response.json()
-# Print Data Response
-print(data["num_sites"], data["num_devices"], data["num_inventory"], data["num_devices_connected"], data["num_devices_disconnected"])
+### Define Function to get Mist API Information
+def fetch_mist():
+	print("Gathering Stats from Mist")
+	response = requests.request("GET", url, headers=headers, data = payload)
+### Print the Output to make sure the GET succeeded
+	print(response.ok)
+	print(response.status_code)
+### Read the output from Mist
+	data = response.text
+### Parse this into JSON
+	parsed = json.loads(data)
+### Print the Output
+# print(json.dumps(parsed, indent=4))
+### Extract the Data
+	Number_of_Sites = parsed["num_sites"]
+	Number_of_Devices_in_Inventory = parsed["num_inventory"]
+	Number_of_Devices_Connected = parsed["num_devices_connected"]
+	Number_of_Devices_Disconnected = parsed["num_devices_disconnected"]
+### Print the Output of each
+	print("Number of Sites:" + str(Number_of_Sites))
+	print("Number of Devices in Inventory:" + str(Number_of_Devices_in_Inventory))
+	print("Number of Devices Connected:" + str(Number_of_Devices_Connected))
+	print("Number of Devices Disconnected:" +str(Number_of_Devices_Disconnected))
+### Schedule fetch_mist
+schedule.every(10).seconds.do(fetch_mist)
+while True:
+	schedule.run_pending()
+	time.sleep(1)
